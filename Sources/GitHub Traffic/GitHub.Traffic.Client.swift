2004c5a11c44c425040b08a299053ec8bea67ae4
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-09-01 17:01:12 +0200

    Initial commit: GitHub API types for Swift
    Path-limited heritage transfer of the Traffic, Stargazers, and Repositories client declarations.
    
    Source-Commit: 63b64124e79519e9da8f457c1429753b542dc0cc

---
+++ Sources/GitHub Traffic/GitHub.Traffic.Client.swift
@@ -0,0 +1,29 @@
+//
+//  GitHub.Traffic.Client.swift
+//  swift-github-types
+//
+//  Created by Coen ten Thije Boonkkamp on 22/08/2025.
+//
+
+import GitHub_Types_Shared
+
+extension GitHub.Traffic {
+    @DependencyClient
+    public struct Client: Sendable {
+        // https://docs.github.com/en/rest/metrics/traffic#get-repository-views
+        @DependencyEndpoint
+        public var views: @Sendable (_ owner: String, _ repo: String, _ per: Per?) async throws -> Views.Response
+        
+        // https://docs.github.com/en/rest/metrics/traffic#get-repository-clones
+        @DependencyEndpoint
+        public var clones: @Sendable (_ owner: String, _ repo: String, _ per: Per?) async throws -> Clones.Response
+        
+        // https://docs.github.com/en/rest/metrics/traffic#get-top-referral-paths
+        @DependencyEndpoint
+        public var paths: @Sendable (_ owner: String, _ repo: String) async throws -> Paths.Response
+        
+        // https://docs.github.com/en/rest/metrics/traffic#get-top-referral-sources
+        @DependencyEndpoint
+        public var referrers: @Sendable (_ owner: String, _ repo: String) async throws -> Referrers.Response
+    }
+}
