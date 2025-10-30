89ba0e8ea5219ff784c05d7dea933507027c154c
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-10-30 15:00:00 +0100

    ci: add Point-Free style code quality tooling
    Path-limited heritage transfer of the Traffic, Stargazers, and Repositories client declarations.
    
    Source-Commit: 3d66b879a36a4d71ad5817e8100b0c8c892e7601

--- Sources/GitHub Stargazers/GitHub.Stargazers.Client.swift
+++ Sources/GitHub Stargazers/GitHub.Stargazers.Client.swift
@@ -5,47 +5,52 @@
 //  Created by Coen ten Thije Boonkkamp on 30/08/2025.
 //
 
-import GitHub_Types_Shared
 import Dependencies
 import DependenciesMacros
+import GitHub_Types_Shared
 
 extension GitHub.Stargazers {
-    @DependencyClient
-    public struct Client: Sendable {
-        // https://docs.github.com/en/rest/activity/starring#list-stargazers
-        @DependencyEndpoint
-        public var list: @Sendable (_ owner: String, _ repo: String, _ request: List.Request?) async throws -> List.Response
-    }
+  @DependencyClient
+  public struct Client: Sendable {
+    // https://docs.github.com/en/rest/activity/starring#list-stargazers
+    @DependencyEndpoint
+    public var list:
+      @Sendable (_ owner: String, _ repo: String, _ request: List.Request?) async throws ->
+        List.Response
+  }
 }
 
 extension GitHub.Stargazers.Client {
-    public func list(owner: String, repo: String) async throws -> GitHub.Stargazers.List.Response {
-        try await self.list(owner, repo, nil)
+  public func list(owner: String, repo: String) async throws -> GitHub.Stargazers.List.Response {
+    try await self.list(owner, repo, nil)
+  }
+
+  public func listAll(
+    owner: String,
+    repo: String
+  ) async throws -> [GitHub.Stargazers.List.Stargazer] {
+    var allStargazers: [GitHub.Stargazers.List.Stargazer] = []
+    var page = 1
+    let perPage = 100  // GitHub's max per page
+
+    while true {
+      let request = GitHub.Stargazers.List.Request(perPage: perPage, page: page)
+      let response = try await self.list(owner, repo, request)
+
+      if response.isEmpty {
+        break
+      }
+
+      allStargazers.append(contentsOf: response)
+
+      // If we got less than perPage, we've reached the end
+      if response.count < perPage {
+        break
+      }
+
+      page += 1
     }
-    
-    public func listAll(owner: String, repo: String) async throws -> [GitHub.Stargazers.List.Stargazer] {
-        var allStargazers: [GitHub.Stargazers.List.Stargazer] = []
-        var page = 1
-        let perPage = 100 // GitHub's max per page
-        
-        while true {
-            let request = GitHub.Stargazers.List.Request(perPage: perPage, page: page)
-            let response = try await self.list(owner, repo, request)
-            
-            if response.isEmpty {
-                break
-            }
-            
-            allStargazers.append(contentsOf: response)
-            
-            // If we got less than perPage, we've reached the end
-            if response.count < perPage {
-                break
-            }
-            
-            page += 1
-        }
-        
-        return allStargazers
-    }
+
+    return allStargazers
+  }
 }
--- Sources/GitHub Traffic/GitHub.Traffic.Client.swift
+++ Sources/GitHub Traffic/GitHub.Traffic.Client.swift
@@ -8,22 +8,25 @@
 import GitHub_Types_Shared
 
 extension GitHub.Traffic {
-    @DependencyClient
-    public struct Client: Sendable {
-        // https://docs.github.com/en/rest/metrics/traffic#get-repository-views
-        @DependencyEndpoint
-        public var views: @Sendable (_ owner: String, _ repo: String, _ per: Per?) async throws -> Views.Response
-        
-        // https://docs.github.com/en/rest/metrics/traffic#get-repository-clones
-        @DependencyEndpoint
-        public var clones: @Sendable (_ owner: String, _ repo: String, _ per: Per?) async throws -> Clones.Response
-        
-        // https://docs.github.com/en/rest/metrics/traffic#get-top-referral-paths
-        @DependencyEndpoint
-        public var paths: @Sendable (_ owner: String, _ repo: String) async throws -> Paths.Response
-        
-        // https://docs.github.com/en/rest/metrics/traffic#get-top-referral-sources
-        @DependencyEndpoint
-        public var referrers: @Sendable (_ owner: String, _ repo: String) async throws -> Referrers.Response
-    }
+  @DependencyClient
+  public struct Client: Sendable {
+    // https://docs.github.com/en/rest/metrics/traffic#get-repository-views
+    @DependencyEndpoint
+    public var views:
+      @Sendable (_ owner: String, _ repo: String, _ per: Per?) async throws -> Views.Response
+
+    // https://docs.github.com/en/rest/metrics/traffic#get-repository-clones
+    @DependencyEndpoint
+    public var clones:
+      @Sendable (_ owner: String, _ repo: String, _ per: Per?) async throws -> Clones.Response
+
+    // https://docs.github.com/en/rest/metrics/traffic#get-top-referral-paths
+    @DependencyEndpoint
+    public var paths: @Sendable (_ owner: String, _ repo: String) async throws -> Paths.Response
+
+    // https://docs.github.com/en/rest/metrics/traffic#get-top-referral-sources
+    @DependencyEndpoint
+    public var referrers:
+      @Sendable (_ owner: String, _ repo: String) async throws -> Referrers.Response
+  }
 }
